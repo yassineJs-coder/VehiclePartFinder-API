ad8e3778ef379c4a2d7d04cc102cbbce2a4e505c
yassineJs-coder <yanosski@gmail.com>
2025-01-14 20:34:28 +0100

    (Config): enhance db conf

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config()
-const uri = process.env.MONGO_URI;
-
-const connectDB = mongoose
-  .connect(uri)
-  .then(() => console.log('MongoDB connected successfully'))
-  .catch((err) => console.error('MongoDB connection error:', err));
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const express = require('express')
-
-require('./db');
+const express = require('express');
+const connectDB = require('./config/db');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-
 // Middleware
 app.use(express.json());
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Connect Database
+connectDB();
+
+// Start Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT} 😅`));
